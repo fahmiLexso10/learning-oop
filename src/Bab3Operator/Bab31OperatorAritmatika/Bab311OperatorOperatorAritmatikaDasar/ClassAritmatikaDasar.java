b1511047d71cae06745cc2aa60b1d67d61b06825
fahmi@skyworx.co.id <fahmi@skyworx.co.id>
2014-11-27 00:09:08 +0700

    tester1

---
+++ src/Bab3Operator/Bab31OperatorAritmatika/Bab311OperatorOperatorAritmatikaDasar/ClassAritmatikaDasar.java
@@ -0,0 +1,17 @@
+package Bab3Operator.Bab31OperatorAritmatika.Bab311OperatorOperatorAritmatikaDasar;
+
+public class ClassAritmatikaDasar {
+
+	public void operasiAritmatika(int a, int b, int c) {
+		System.out.println("===Operasi Aritmatika pada tipe integer===");
+		System.out.println("Nilai a : " + a);
+		System.out.println("Nilai b : " + b);
+		System.out.println("Nilai c : " + c);
+		System.out.println();
+		System.out.println("Nilai penambahan a + b : " + (a + b));
+		System.out.println("Nilai pengurangan a - b : " + (a - b));
+		System.out.println("Nilai perkalian a * a : " + (a * a));
+		System.out.println("Nilai pembagian a / b : " + (a / b));
+		System.out.println("Nilai negasi b : " + (-b));
+	}
+}
---
+++ src/Bab3Operator/Bab31OperatorAritmatika/Bab311OperatorOperatorAritmatikaDasar/DemoAritmatikaDasar.java
@@ -0,0 +1,10 @@
+package Bab3Operator.Bab31OperatorAritmatika.Bab311OperatorOperatorAritmatikaDasar;
+
+public class DemoAritmatikaDasar {
+	public static void main(String[] args) {
+
+		ClassAritmatikaDasar cad = new ClassAritmatikaDasar();
+		cad.operasiAritmatika(2, 5, 6);
+		
+	}
+}
